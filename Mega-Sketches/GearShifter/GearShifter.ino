3dda44fc3baafe4651d68ce295cb27d80658b161
Alex Schlich <a.schlich@sigmatech.de>
2016-12-02 17:14:15 +0100

    Adding sketches for arduino mega, adding wiring-diagram

---
+++ Mega-Sketches/GearShifter/GearShifter.ino
@@ -0,0 +1,127 @@
+//
+// code based on source code by moz from youtube channel LogMaker
+//
+// expanded and modified by #w0rk
+// 
+
+
+#define ROW_1 22
+#define ROW_2 23
+#define ROW_3 24
+#define ROW_4 25
+#define ROW_5 26
+#define ROW_6 27
+#define ROW_7 28
+#define ROW_8 29
+
+#define COL_1 30
+#define COL_2 31
+#define COL_3 32
+#define COL_4 33
+#define COL_5 34
+#define COL_6 35
+#define COL_7 36
+#define COL_8 37
+
+const byte rows[] = {
+    ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7, ROW_8
+};
+
+// The display buffer
+// (1 = ON, 0 = OFF)
+byte gear[9][8] = {
+	{B00000000,B00111100,B00100100,B00101100,B00110000,B00101000,B00100100,B00000000},		// R
+	{B00000000,B00110000,B00101000,B00100100,B00100100,B00100100,B00100100,B00000000},		// N
+	{B00000000,B00001000,B00011000,B00101000,B00001000,B00001000,B00001000,B00000000},		// 1
+	{B00000000,B00111100,B00100100,B00000100,B00111100,B00100000,B00111100,B00000000},		// 2
+	{B00000000,B00111100,B00000100,B00000100,B00111100,B00000100,B00111100,B00000000},		// 3
+	{B00000000,B00100100,B00100100,B00100100,B00111100,B00000100,B00000100,B00000000},		// 4
+	{B00000000,B00111100,B00100000,B00100000,B00111100,B00000100,B00111100,B00000000},		// 5
+	{B00000000,B00111100,B00100000,B00100000,B00111100,B00100100,B00111100,B00000000},		// 6
+	{B00000000,B00111100,B00000100,B00000100,B00000100,B00000100,B00000100,B00000000}, 		// 7
+};
+
+
+// setting variables for loop function
+
+int GearCount = 1;
+int ButtonGearUp = A0;
+int ButtonGearDown = A1;
+
+
+void setup() {
+    // Open serial port
+    Serial.begin(9600);
+    
+    // Set pins to OUTPUT
+    for (byte i = 22; i <= 37; i++)
+    pinMode(i, OUTPUT);
+
+    // set gearshifter pins to input
+    pinMode(ButtonGearUp, INPUT);
+    pinMode(ButtonGearDown, INPUT);
+    
+}
+
+
+void loop() {
+	int GearUp = digitalRead(ButtonGearUp);
+	int GearDown = digitalRead(ButtonGearDown);
+
+        //Serial.print("DEBUG 1.0: GearCount =");
+        //Serial.println(GearCount);
+
+	if ((GearUp == 1) && (GearDown == 0) && (GearCount < 8)) {	        // GearUp, if possible
+		GearCount ++;
+                delay(175);
+
+        //Serial.print("DEBUG 1.1: GearCount =");
+        //Serial.println(GearCount);
+  
+	}	
+
+	if ((GearDown == 1) && (GearDown == 1) && (GearCount > 0 )) {	        // GearDown, if possible
+		GearCount --;
+                delay(175);
+
+        //Serial.print("DEBUG 1.2: GearCount =");
+        //Serial.println(GearCount);
+
+	} 
+ 
+	drawScreen(gear[GearCount]);
+    
+        //Serial.print("DEBUG 2: GearCount =");
+        //Serial.println(GearCount);
+
+}
+
+
+void drawScreen(byte buffer2[]){
+     
+    
+   // Turn on each row in series
+    for (byte i = 0; i < 8; i++) {
+        setColumns(buffer2[i]); // Set columns for this specific row
+        
+        digitalWrite(rows[i], HIGH);
+        delay(1); // Set this to 50 or 100 if you want to see the multiplexing effect!
+        digitalWrite(rows[i], LOW);
+        
+    }
+}
+
+
+void setColumns(byte b) {
+    digitalWrite(COL_1, (~b >> 0) & 0x01); // Get the 1st bit: 10000000
+    digitalWrite(COL_2, (~b >> 1) & 0x01); // Get the 2nd bit: 01000000
+    digitalWrite(COL_3, (~b >> 2) & 0x01); // Get the 3rd bit: 00100000
+    digitalWrite(COL_4, (~b >> 3) & 0x01); // Get the 4th bit: 00010000
+    digitalWrite(COL_5, (~b >> 4) & 0x01); // Get the 5th bit: 00001000
+    digitalWrite(COL_6, (~b >> 5) & 0x01); // Get the 6th bit: 00000100
+    digitalWrite(COL_7, (~b >> 6) & 0x01); // Get the 7th bit: 00000010
+    digitalWrite(COL_8, (~b >> 7) & 0x01); // Get the 8th bit: 00000001
+    
+    // If the polarity of your matrix is the opposite of mine
+    // remove all the '~' above.
+}
