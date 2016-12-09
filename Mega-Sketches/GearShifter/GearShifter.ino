10fcd80b9ec2b498a5e7d6099cb49c7396461dbb
cuba <cubaallstar@web.de>
2016-12-09 03:32:00 +0100

    adding gear shift indicator

--- Mega-Sketches/GearShifter/GearShifter.ino
+++ Mega-Sketches/GearShifter/GearShifter.ino
@@ -23,8 +23,19 @@
 #define COL_7 36
 #define COL_8 37
 
+#define LED_01 40
+#define LED_02 41
+#define LED_03 42
+#define LED_04 44
+#define LED_05 45
+#define LED_06 48
+
 const byte rows[] = {
     ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7, ROW_8
+};
+
+const byte leds[] = {
+    LED_01, LED_02, LED_03, LED_04, LED_05, LED_06
 };
 
 // The display buffer
@@ -41,65 +52,80 @@ byte gear[9][8] = {
 	{B00000000,B00111100,B00000100,B00000100,B00000100,B00000100,B00000100,B00000000}, 		// 7
 };
 
-
 // setting variables for loop function
-
-int GearCount = 1;
-int ButtonGearUp = A0;
-int ButtonGearDown = A1;
+    int GearCount = 1;
+    int ButtonGearUp = A0;
+    int ButtonGearDown = A1;
+    int PotPin = A2;
+    int valPot = 0;
+    int potenzometer = 0;
 
 
 void setup() {
-    // Open serial port
+
+
+  // Open serial port
     Serial.begin(9600);
     
     // Set pins to OUTPUT
-    for (byte i = 22; i <= 37; i++)
+    for (byte i = 22; i <= 53; i++)
     pinMode(i, OUTPUT);
+
+    for (byte j = 8; j <= 12; j++)
+    pinMode(j,OUTPUT);
+
 
     // set gearshifter pins to input
     pinMode(ButtonGearUp, INPUT);
     pinMode(ButtonGearDown, INPUT);
-    
+    pinMode(PotPin, INPUT);
+   
 }
 
 
 void loop() {
-	int GearUp = digitalRead(ButtonGearUp);
-	int GearDown = digitalRead(ButtonGearDown);
 
-        //Serial.print("DEBUG 1.0: GearCount =");
+    	int GearUp = digitalRead(ButtonGearUp);
+	int GearDown = digitalRead(ButtonGearDown);
+        valPot = analogRead(PotPin);
+
+        //Serial.print("DEBUG 0.0: valPot = ");
+        //Serial.println(valPot);
+
+        //Serial.print("DEBUG 1.0: GearCount = ");
         //Serial.println(GearCount);
 
-	if ((GearUp == 1) && (GearDown == 0) && (GearCount < 8)) {	        // GearUp, if possible
+	if ((GearUp == 1) && (GearDown == 0) && (GearCount < 8) || (valPot >= 600) && (GearCount < 8)) {	        // GearUp, if possible
 		GearCount ++;
                 delay(175);
 
-        //Serial.print("DEBUG 1.1: GearCount =");
+
+        //Serial.print("DEBUG 1.1: GearCount = ");
         //Serial.println(GearCount);
   
 	}	
 
-	if ((GearDown == 1) && (GearDown == 1) && (GearCount > 0 )) {	        // GearDown, if possible
+	if ((GearDown == 1) && (GearDown == 1) && (GearCount > 0 ) || (valPot <= 10) && (GearCount > 0 )) {	        // GearDown, if possible
 		GearCount --;
                 delay(175);
 
-        //Serial.print("DEBUG 1.2: GearCount =");
+        //Serial.print("DEBUG 1.2: GearCount = ");
         //Serial.println(GearCount);
 
 	} 
- 
+        
 	drawScreen(gear[GearCount]);
+        flash();
+
     
-        //Serial.print("DEBUG 2: GearCount =");
+        //Serial.print("DEBUG 2: GearCount = ");
         //Serial.println(GearCount);
 
 }
 
 
 void drawScreen(byte buffer2[]){
-     
-    
+        
    // Turn on each row in series
     for (byte i = 0; i < 8; i++) {
         setColumns(buffer2[i]); // Set columns for this specific row
@@ -109,6 +135,58 @@ void drawScreen(byte buffer2[]){
         digitalWrite(rows[i], LOW);
         
     }
+}
+
+void flash() {
+
+  potenzometer = analogRead(PotPin);
+ 
+ if (potenzometer <= 100) {
+    digitalWrite (LED_01,HIGH);
+    digitalWrite (LED_02,LOW);
+    digitalWrite (LED_03,LOW);
+    digitalWrite (LED_04,LOW);
+    digitalWrite (LED_05,LOW);
+ }
+ else if (potenzometer <= 200) {
+    digitalWrite (LED_01,HIGH);
+    digitalWrite (LED_02,HIGH);
+    digitalWrite (LED_03,LOW);
+    digitalWrite (LED_04,LOW);
+    digitalWrite (LED_05,LOW);
+ } 
+ else if (potenzometer <= 300) {
+    digitalWrite (LED_01,HIGH);
+    digitalWrite (LED_02,HIGH);
+    digitalWrite (LED_03,HIGH);
+    digitalWrite (LED_04,LOW);
+    digitalWrite (LED_05,LOW);
+ }
+ else if (potenzometer <= 400) {
+    digitalWrite (LED_01,HIGH);
+    digitalWrite (LED_02,HIGH);
+    digitalWrite (LED_03,HIGH);
+    digitalWrite (LED_04,HIGH);
+    digitalWrite (LED_05,LOW);
+ }
+ else if (potenzometer <= 450) {
+    digitalWrite (LED_01,HIGH);
+    digitalWrite (LED_02,HIGH);
+    digitalWrite (LED_03,HIGH);
+    digitalWrite (LED_04,HIGH);
+    digitalWrite (LED_05,HIGH);
+ }
+ else if (potenzometer <= 590){
+    digitalWrite (LED_01,LOW);
+    digitalWrite (LED_02,LOW);
+    digitalWrite (LED_03,LOW);
+    digitalWrite (LED_04,LOW);
+    digitalWrite (LED_05,LOW);
+    
+    digitalWrite (LED_06,HIGH);
+    delay(45);
+    digitalWrite (LED_06,LOW);
+ }   
 }
 
 
